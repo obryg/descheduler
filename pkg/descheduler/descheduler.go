6d0c12972e0f43bcb0e64e3719cca2d766c1646c
obryg <obryg@softserveinc.com>
2019-01-03 12:12:02 +0200

    initial repo - third time

---
+++ pkg/descheduler/descheduler.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package descheduler
+
+import (
+	"fmt"
+
+	"github.com/golang/glog"
+
+	"github.com/kubernetes-incubator/descheduler/cmd/descheduler/app/options"
+	"github.com/kubernetes-incubator/descheduler/pkg/descheduler/client"
+	eutils "github.com/kubernetes-incubator/descheduler/pkg/descheduler/evictions/utils"
+	nodeutil "github.com/kubernetes-incubator/descheduler/pkg/descheduler/node"
+	"github.com/kubernetes-incubator/descheduler/pkg/descheduler/strategies"
+)
+
+func Run(rs *options.DeschedulerServer) error {
+
+	rsclient, err := client.CreateClient(rs.KubeconfigFile)
+	if err != nil {
+		return err
+	}
+	rs.Client = rsclient
+
+	deschedulerPolicy, err := LoadPolicyConfig(rs.PolicyConfigFile)
+	if err != nil {
+		return err
+	}
+	if deschedulerPolicy == nil {
+		return fmt.Errorf("deschedulerPolicy is nil")
+	}
+
+	evictionPolicyGroupVersion, err := eutils.SupportEviction(rs.Client)
+	if err != nil || len(evictionPolicyGroupVersion) == 0 {
+		return err
+	}
+
+	stopChannel := make(chan struct{})
+	nodes, err := nodeutil.ReadyNodes(rs.Client, rs.NodeSelector, stopChannel)
+	if err != nil {
+		return err
+	}
+
+	if len(nodes) <= 1 {
+		glog.V(1).Infof("The cluster size is 0 or 1 meaning eviction causes service disruption or degradation. So aborting..")
+		return nil
+	}
+
+	nodePodCount := strategies.InitializeNodePodCount(nodes)
+	strategies.RemoveDuplicatePods(rs, deschedulerPolicy.Strategies["RemoveDuplicates"], evictionPolicyGroupVersion, nodes, nodePodCount)
+	strategies.LowNodeUtilization(rs, deschedulerPolicy.Strategies["LowNodeUtilization"], evictionPolicyGroupVersion, nodes, nodePodCount)
+	strategies.RemovePodsViolatingInterPodAntiAffinity(rs, deschedulerPolicy.Strategies["RemovePodsViolatingInterPodAntiAffinity"], evictionPolicyGroupVersion, nodes, nodePodCount)
+	strategies.RemovePodsViolatingNodeAffinity(rs, deschedulerPolicy.Strategies["RemovePodsViolatingNodeAffinity"], evictionPolicyGroupVersion, nodes, nodePodCount)
+
+	return nil
+}
---
+++ pkg/descheduler/strategies/duplicates_test.go
@@ -0,0 +1,85 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package strategies
+
+import (
+	"testing"
+
+	"github.com/kubernetes-incubator/descheduler/test"
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/fake"
+	core "k8s.io/client-go/testing"
+)
+
+//TODO:@ravisantoshgudimetla This could be made table driven.
+func TestFindDuplicatePods(t *testing.T) {
+	node := test.BuildTestNode("n1", 2000, 3000, 10)
+	p1 := test.BuildTestPod("p1", 100, 0, node.Name)
+	p2 := test.BuildTestPod("p2", 100, 0, node.Name)
+	p3 := test.BuildTestPod("p3", 100, 0, node.Name)
+	p4 := test.BuildTestPod("p4", 100, 0, node.Name)
+	p5 := test.BuildTestPod("p5", 100, 0, node.Name)
+	p6 := test.BuildTestPod("p6", 100, 0, node.Name)
+	p7 := test.BuildTestPod("p7", 100, 0, node.Name)
+	p8 := test.BuildTestPod("p8", 100, 0, node.Name)
+	p9 := test.BuildTestPod("p9", 100, 0, node.Name)
+
+	// All the following pods expect for one will be evicted.
+	p1.ObjectMeta.OwnerReferences = test.GetReplicaSetOwnerRefList()
+	p2.ObjectMeta.OwnerReferences = test.GetReplicaSetOwnerRefList()
+	p3.ObjectMeta.OwnerReferences = test.GetReplicaSetOwnerRefList()
+	p8.ObjectMeta.OwnerReferences = test.GetReplicaSetOwnerRefList()
+	p9.ObjectMeta.OwnerReferences = test.GetReplicaSetOwnerRefList()
+
+	// The following 4 pods won't get evicted.
+	// A daemonset.
+	p4.ObjectMeta.OwnerReferences = test.GetDaemonSetOwnerRefList()
+	// A pod with local storage.
+	p5.ObjectMeta.OwnerReferences = test.GetNormalPodOwnerRefList()
+	p5.Spec.Volumes = []v1.Volume{
+		{
+			Name: "sample",
+			VolumeSource: v1.VolumeSource{
+				HostPath: &v1.HostPathVolumeSource{Path: "somePath"},
+				EmptyDir: &v1.EmptyDirVolumeSource{
+					SizeLimit: resource.NewQuantity(int64(10), resource.BinarySI)},
+			},
+		},
+	}
+	// A Mirror Pod.
+	p6.Annotations = test.GetMirrorPodAnnotation()
+	// A Critical Pod.
+	p7.Namespace = "kube-system"
+	p7.Annotations = test.GetCriticalPodAnnotation()
+	expectedEvictedPodCount := 2
+	fakeClient := &fake.Clientset{}
+	fakeClient.Fake.AddReactor("list", "pods", func(action core.Action) (bool, runtime.Object, error) {
+		return true, &v1.PodList{Items: []v1.Pod{*p1, *p2, *p3, *p4, *p5, *p6, *p7, *p8, *p9}}, nil
+	})
+	fakeClient.Fake.AddReactor("get", "nodes", func(action core.Action) (bool, runtime.Object, error) {
+		return true, node, nil
+	})
+	npe := nodePodEvictedCount{}
+	npe[node] = 0
+	podsEvicted := deleteDuplicatePods(fakeClient, "v1", []*v1.Node{node}, false, npe, 2)
+	if podsEvicted != expectedEvictedPodCount {
+		t.Errorf("Unexpected no of pods evicted")
+	}
+
+}
